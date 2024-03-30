5f79fc9ac80707b967cfa3c7120578fc61e6dda2
Khushbakht <khushbakht.gillani@dcmail.ca>
2024-03-29 17:36:26 -0700

    part 2 done

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -6,10 +6,12 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 /* Declaring the alternative text for each image file */
 const altText = ['A beautiful landscape', 
 'A beautiful landscape', 'A beautiful landscape',
  'A beautiful landscape', 'A beautiful landscape'];
+
 /* Looping through images */
 for (let i = 0; i < imageArray.length; i++) {
 const newImage = document.createElement('img');
@@ -20,6 +22,7 @@ newImage.addEventListener('click', (e) => {
     displayedImage.src = e.target.src;
 });
 }
+
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
