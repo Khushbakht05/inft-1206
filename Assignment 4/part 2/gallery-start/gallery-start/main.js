5f141beab7d78d28ae7246df90089d8e9ab2855e
Khushbakht <khushbakht.gillani@dcmail.ca>
2024-03-29 17:49:39 -0700

    removed extra files from part 2

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -1,34 +1,42 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
-const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-/* Declaring the alternative text for each image file */
-const altText = ['A beautiful landscape', 
-'A beautiful landscape', 'A beautiful landscape',
- 'A beautiful landscape', 'A beautiful landscape'];
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-for (let i = 0; i < imageArray.length; i++) {
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-newImage.addEventListener('click', (e) => {
-    displayedImage.src = e.target.src;
-});
+  function thumbnailLoop() {
+  var i;
+  for (i = 0; i < 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', "images/pic" + (i+1) + ".jpg");
+  thumbBar.appendChild(newImage);
+}
 }
 
+thumbnailLoop();
+
+/* Making the clicked thumbnail the large image */
+
+thumbBar.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName == "IMG") {
+    displayedImage.src = e.target.src;
+}
+}); 
+
+  
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
-    if (btnClass === 'dark') {
-    
-    } else {
-        
-    }
+
+btn.addEventListener('click', (e) => {
+  if (e.target.className == 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
+
+ else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
 });
